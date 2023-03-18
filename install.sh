9e5035ce19325c947b4d0216ab976865890ba86a
littleblack111 <jerrylam110209@gmail.com>
2023-03-18 21:17:51 +0800

    set default path, finshed install,sh

--- install.sh
+++ install.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
+printf "Please install dependencys(if missing): dbus pkgconfig cargo"
+sleep 1
+
 printf "Downloading file...\n"
 #download to $HOME/.cache
+version=1.0
 downloaction=$HOME/.cache
 curl -L https://github.com/Insert5StarName/NekoMC/archive/refs/tags/1.0.tar.gz -o $downloaction/NekoMC.tar.gz
+tar xvzf $downloaction/NekoMC
+printf "building..."
+cd NekoMC-$version
+cargo build
 
 
 # ask user where to put(waybar/polybar)
 printf "Where do you want to store the file: "
 read -r location
-cp $downloaction/NekoMC.tar.gz "$location" && rm -rf $downlocation/NekoMC.tar.gz
+#mv $downloaction/NekoMC.tar.gz "$location"
+printf "claiming sudo privliage to install bin to /usr/bin"
+sudo mv "NekoMC-$version/target/debug/NekoMC" /usr/bin
 printf "if you are using polybar, remenber to put this to your polybar config:\n\n\n"
 printf "\n[module/musics]
 type = custom/script
@@ -28,4 +38,9 @@ printf '\n"custom/NekoMC": {
           "on-scroll-down":"$HOME/.config/waybar/NekoMC --previous",
           "escape" : "true",
  },\n'
+
+ sudo rm -rf $downloaction/NekoMC
+ sudo rm -rf NekoMC-$version
+
+ printf "\n\nIf no errors, then well, NekoMC install sussfully!!!\n"
 
